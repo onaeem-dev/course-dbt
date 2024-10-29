8352ed4daaa975392609874baf821ae593db434b
onaeem-dev <naeemosama921@gmail.com>
2024-10-29 02:22:59 +0000

    project 3

--- greenery/models/marts/product/fact_page_views.sql
+++ greenery/models/marts/product/fact_page_views.sql
@@ -12,16 +12,21 @@ session_timing_agg as (
     select * from {{ ref('int_session_timings') }}
 )
 
+{% set event_types = dbt_utils.get_column_values(
+    table=ref('stg_postgres__events'), 
+    column='event_type'
+) %}
+
+
 select
     e.session_id,
     e.user_id,
     coalesce(e.product_id, oi.product_id) as product_id,
     session_started_at,
     session_ended_at,
-    sum(case when e.event_type = 'page_view' then 1 else 0 end) as page_view,
-    sum(case when e.event_type = 'add_to_cart' then 1 else 0 end) as add_to_carts,
-    sum(case when e.event_type = 'checkout' then 1 else 0 end) as checkouts,
-    sum(case when e.event_type = 'package_shipped' then 1 else 0 end) as packages_shipped,
+    {% for event_type in event_types %}
+    {{ sum_of('e.event_type', event_type) }} as {{ event_type }}s,
+    {% endfor %} 
     datediff('minute', session_started_at, session_ended_at) as session_length_minutes
 from events e
 left join order_items oi
